902209c2633427eb3b7e504a423afec4deffdb5f
Michellelyk <myl19@ic.ac.uk>
2019-10-17 15:49:38 +0100

    Adding file

---
+++ Week-2/Week 2 Number Guessing Game.py
@@ -0,0 +1,8 @@
+# This program asks the user to guess a number between 1 and 10.
+import random
+print('Guess which number I am thinking of now. It is between 1 and 10.')
+yourNumber=input()
+print(int(yourNumber)==random.randint(1,10))
+
+
+
